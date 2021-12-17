1c2ec42c5dd7a7c3f6968463717afa46ea878b4a
Tommy Nguyen <tommy.nguyen@pc260.na.corp.samsungelectronics.net>
2021-12-16 21:34:09 -0500

    Finished styling progress bar, yay :)

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
 import styled from "styled-components";
 
@@ -7,18 +6,33 @@ import VisuallyHidden from "../VisuallyHidden";
 
 const PROGRESS_BAR_WIDTH = 370;
 
-const SIZES = {
+const WRAPPER_SIZES = {
   small: {
     "--height": "8px",
+    "--width": `${PROGRESS_BAR_WIDTH}px`,
     "--padding": "initial",
   },
   medium: {
     "--height": "12px",
+    "--width": `${PROGRESS_BAR_WIDTH}px`,
     "--padding": "initial",
   },
   large: {
     "--height": "24px",
+    "--width": `${PROGRESS_BAR_WIDTH + 8}px`,
     "--padding": "4px",
+  },
+};
+
+const LOADING_BAR_SIZES = {
+  small: {
+    "--height": "8px",
+  },
+  medium: {
+    "--height": "12px",
+  },
+  large: {
+    "--height": "16px",
   },
 };
 
@@ -26,11 +40,16 @@ const ProgressBar = ({ value, size }) => {
   const progress = (value / 100) * PROGRESS_BAR_WIDTH;
   const isComplete = progress === PROGRESS_BAR_WIDTH;
 
-  const styles = SIZES[size];
+  const wrapperStyles = WRAPPER_SIZES[size];
+  const loadingBarStyles = LOADING_BAR_SIZES[size];
 
   return (
-    <Wrapper style={styles}>
-      <LoadingBar progress={progress} isComplete={isComplete}></LoadingBar>
+    <Wrapper style={wrapperStyles}>
+      <LoadingBar
+        style={loadingBarStyles}
+        progress={progress}
+        isComplete={isComplete}
+      ></LoadingBar>
     </Wrapper>
   );
 };
@@ -39,20 +58,16 @@ export default ProgressBar;
 
 const Wrapper = styled.div`
   background-color: ${COLORS.transparentGray15};
-  width: 370px;
+  width: var(--width);
   height: var(--height);
   border-radius: 4px;
-  padding: 4px;
+  padding: var(--padding);
   position: relative;
 `;
 
 const LoadingBar = styled.div`
   background-color: ${COLORS.primary};
   width: ${(props) => props.progress + "px"};
-  height: 16px;
-  /* padding: 4px; */
+  height: var(--height);
   border-radius: ${(props) => (props.isComplete ? "4px" : "4px 0px 0px 4px")};
-  /* position: relative;
-  top: 4px;
-  left: 4px; */
 `;
