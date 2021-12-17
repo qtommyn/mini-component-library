eb460f60c9ee17f99d4ac60148ad3d9725500cd8
Tommy Nguyen <tommy.nguyen@pc260.na.corp.samsungelectronics.net>
2021-12-16 20:18:47 -0500

    Added styles to progress bar component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,58 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const PROGRESS_BAR_WIDTH = 370;
+
+const SIZES = {
+  small: {
+    "--height": "8px",
+    "--padding": "initial",
+  },
+  medium: {
+    "--height": "12px",
+    "--padding": "initial",
+  },
+  large: {
+    "--height": "24px",
+    "--padding": "4px",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const progress = (value / 100) * PROGRESS_BAR_WIDTH;
+  const isComplete = progress === PROGRESS_BAR_WIDTH;
+
+  const styles = SIZES[size];
+
+  return (
+    <Wrapper style={styles}>
+      <LoadingBar progress={progress} isComplete={isComplete}></LoadingBar>
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  width: 370px;
+  height: var(--height);
+  border-radius: 4px;
+  padding: 4px;
+  position: relative;
+`;
+
+const LoadingBar = styled.div`
+  background-color: ${COLORS.primary};
+  width: ${(props) => props.progress + "px"};
+  height: 16px;
+  /* padding: 4px; */
+  border-radius: ${(props) => (props.isComplete ? "4px" : "4px 0px 0px 4px")};
+  /* position: relative;
+  top: 4px;
+  left: 4px; */
+`;
